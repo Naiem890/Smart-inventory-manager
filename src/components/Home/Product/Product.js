cbc0ebf11b925b1914325dd3e6548086a868e3e9
Naiem890 <solaiman.naiem890@gmail.com>
2022-04-30 01:43:36 +0600

    all products loaded form server dynamically

--- src/components/Home/Product/Product.js
+++ src/components/Home/Product/Product.js
@@ -1,29 +1,35 @@
 import React from "react";
 
-const Product = () => {
+const Product = ({ product }) => {
+  const {
+    productName,
+    productImage,
+    productDescription,
+    productPrice,
+    productStock,
+  } = product;
+
   return (
     <div>
       <div className="max-w-sm bg-white rounded-xl border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
         <div className="bg-slate-200 m-5 rounded-xl">
-          <img
-            className="rounded-t-lg px-12 py-6"
-            src="https://i.ibb.co/tq34Y8F/toppng-com-headphone-669x846.png"
-            alt=""
-          />
+          <img className="rounded-t-lg px-12 py-6" src={productImage} alt="" />
         </div>
         <div className="p-5 pt-0">
           <div>
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Product Name Here
+            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+              {productName}
             </h5>
           </div>
           <p className="mb-3  text-gray-500 dark:text-gray-400">
-            Here are the biggest enterprise technology
+            {productDescription}
           </p>
           <div className="flex justify-between align-middle mb-5">
-            <h3 className="text-2xl font-bold text-blue-500">$230</h3>
+            <h3 className="text-2xl font-bold text-blue-500">
+              ${productPrice}
+            </h3>
             <p className="text-gray-500">
-              Stock: <span>20</span>
+              Stock: <span>{productStock}</span>
             </p>
           </div>
           <div className=" px-2">
--- src/components/Home/Products/Products.js
+++ src/components/Home/Products/Products.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Product from "../Product/Product";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch(`http://localhost:5000/products`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <h2 className="text-5xl font-kanit mb-10 capitalize text-center">
         Manage your product
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
-        <Product></Product>
-        <Product></Product>
-        <Product></Product>
-        <Product></Product>
+        {products.map((product) => (
+          <Product key={product._id} product={product}></Product>
+        ))}
       </div>
     </div>
   );
