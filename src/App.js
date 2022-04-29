5f08dcbd04920f3c1f68c62bf2e5f8fce6349e1a
Naiem890 <solaiman.naiem890@gmail.com>
2022-04-29 20:13:31 +0600

    footer added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/shared/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Footer from "./components/shared/Header/Footer/Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home></Home>}></Route>
         </Routes>
       </BrowserRouter>
+      <Footer></Footer>
     </div>
   );
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,12 +3,12 @@ import React from "react";
 const Home = () => {
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-24 my-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-24 my-20">
         <div>
-          <h1 className="text-5xl font-kanit mb-10">
-            Manager your headphone inventory from our smart manager
+          <h1 className="text-6xl font-kanit mb-6 capitalize">
+            Manager your headphone inventory smartly
           </h1>
-          <p className="text-gray-500">
+          <p className="text-gray-500 text-lg">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione
             maiores voluptates quibusdam illum soluta vero ducimus dolorem
             quidem labore quaerat, dolore voluptas sed doloremque quisquam non
